f364a31e0b40c47db1444877270b2de190b39826
jessefokkinga <68752006+jessefokkinga@users.noreply.github.com>
2022-08-14 18:08:25 +0200

    Update inference.py

--- inference.py
+++ inference.py
@@ -15,38 +15,14 @@ from processing import perform_preprocessing, prepare_submission
 train_df, targets, targets_nonscored, test_df = perform_preprocessing()
 X_test = test_df.values
 
-# Define parameters for during training procedure
-epochs = 24,
-batch_size = 128,
-lr = 1e-3,
-early_stopping_steps = 10,
-pct_start = 0.1
-early_stop = True,
-in_size = len(train_df.columns),
-out_size = len(targets.columns),
-out_size_full = len(targets.columns) + len(targets_nonscored.columns),
+# Define parameters relevant during inference
+batch_size = 128
+in_size = len(train_df.columns)
+out_size = len(targets.columns)
 hidden_size = 1500
 number_of_splits = 10
 seeds = [0,42,1337,666,7]
 device = ('cuda' if torch.cuda.is_available() else 'cpu')
-weight_decay = {'all_targets': 1e-5, 'transfer_learn': 3e-6, 'standard': 1e-5}
-max_lr = {'all_targets': 1e-2, 'transfer_learn': 3e-3,'standard': 1e-2}
-div_factor = {'all_targets': 1e3, 'transfer_learn': 1e2, 'standard':1e3}
-
-tabnet_params = dict(
-    n_d = 32,
-    n_a = 32,
-    n_steps = 1,
-    gamma = 1.3,
-    lambda_sparse = 0,
-    optimizer_fn = optim.Adam,
-    optimizer_params = dict(lr = 2e-2, weight_decay = 1e-5),
-    mask_type = "entmax",
-    scheduler_params = dict(
-        mode = "min", patience = 5, min_lr = 1e-5, factor = 0.9),
-    scheduler_fn = ReduceLROnPlateau,
-    verbose = 10
-)
 
 test_preds_tabnet = []
 test_preds_nn = []
@@ -57,16 +33,19 @@ for seed in seeds:
     for fold_nb, (train_idx, val_idx) in enumerate(mskf.split(train_df, targets)):
         print("FOLDS: ", fold_nb)
 
-        model = TabNetRegressor(**tabnet_params)
+        # Generate TabNet predictions
+        model = TabNetRegressor()
         model.load_model("trained_models/tabnet_" + f"fold_{fold_nb}_{seed}.zip")
         preds_test = model.predict(X_test)
         preds_test = 1 / (1 + np.exp(-preds_test))
 
         test_preds_tabnet.append(preds_test)
 
+        # Load datasets via PyTorch dataloader
         test_ds = TabularDatasetTest(X_test)
         test_dl = DataLoader(test_ds, batch_size=batch_size, shuffle=False)
 
+        # Generate predictions for our simple neural network
         model = simple_neural_net(num_features=in_size, num_targets=out_size,
                                  hidden_size=hidden_size)
 
@@ -83,6 +62,7 @@ for seed in seeds:
 
         test_preds_nn.append(preds_test)
 
+        # Generate predictions from our transfer learning model
         model = transfer_learning_neural_net(num_features=in_size, num_targets=out_size,
                                  hidden_size=hidden_size)
 
